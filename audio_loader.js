bb84635bdcd673d13195995d2adcebac95e8c32f
Malte Gruber <malte_gruber@hotmail.com>
2018-11-05 00:47:42 +0100

    Added progress bar
    And did some cleanup.

--- audio_loader.js
+++ audio_loader.js
@@ -4,13 +4,16 @@ var context=new AudioContext();
 function onError(e){
     console.error("error",e)
 }
-function loadSound(url){
+function loadSound(url,onloadCallback,loadingProgressCb){
     var request=new XMLHttpRequest();
     request.open('GET',url,true);
     request.responseType='arraybuffer';
-
+    
+    request.onprogress=(e)=>{loadingProgressCb(0.9*Math.round(100*e.loaded/e.total))}
+    
     request.onload=function(){
         context.decodeAudioData(request.response,function(buffer){
+            loadingProgressCb(100);
             onloadCallback(buffer);
         },onError);
     }
--- voyager.js
+++ voyager.js
@@ -1,5 +1,5 @@
 
-var leftChannel={
+let leftChannel={
     go:true,
     offset:3000000,
     samples:null,
@@ -7,7 +7,7 @@ var leftChannel={
     canvasName:"imgCanvasLeft",
     plotName:"plotLeft"
 };
-var rightChannel={
+let rightChannel={
     go:true,
     offset:3000000,
     samples:null,
@@ -17,81 +17,80 @@ var rightChannel={
 };
 
 function plot(buffer,offset,len,marker0,marker1,name){
-    var c=document.getElementById(name);
-    var ctx=c.getContext("2d");
-    var zoom=200;
-    var W=c.width;
-    var H=c.height;
-    var center=H/2;
-    var x=0;
-    var dx=W/len;
+    let c=document.getElementById(name);
+    let ctx=c.getContext("2d");
+    let zoom=200;
+    let W=c.width;
+    let H=c.height;
+    let center=H/2;
+    let x=0;
+    let dx=W/len;
     ctx.clearRect(0,0,W,H);
     ctx.beginPath();
     ctx.moveTo(x,center);
-    var plotStart=-140;
-    for(var i=0;i<len;i++){
+    let plotStart=-140;
+    for(let i=0;i<len;i++){
         x+=dx;
         ctx.lineTo(x,center-buffer[i+offset+plotStart]*(zoom));
     }
 
     /*Draw some markers
-   ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(0,center-marker0*(zoom));
-    ctx.lineTo(W,center-marker0*(zoom));
-    ctx.moveTo(0,center-marker1*(zoom));
-    ctx.lineTo(W,center-marker1*(zoom));
-    ctx.moveTo(W-plotStart*dx,0);
-    ctx.lineTo(W-plotStart*dx,H);*/
+     ctx.stroke();
+     
+     ctx.beginPath();
+     ctx.moveTo(0,center-marker0*(zoom));
+     ctx.lineTo(W,center-marker0*(zoom));
+     ctx.moveTo(0,center-marker1*(zoom));
+     ctx.lineTo(W,center-marker1*(zoom));
+     ctx.moveTo(W-plotStart*dx,0);
+     ctx.lineTo(W-plotStart*dx,H);*/
     ctx.stroke();
 }
 
 /*Finds the offset at the next sync pulse*/
-function nextLine(obj){
+function nextLine(channel){
     //Statemachine
-    var lowCount=0;
-    var triggerCount=10;
-    var lowLevel=0;
-    var highLevelReachedCounter=0;
-    obj.offset+=300;
+    let lowCount=0;
+    let triggerCount=10;
+    let lowLevel=0;
+    let highLevelReachedCounter=0;
+    channel.offset+=300;
+    let max_interimage_samples=10000; /*Not actually 10000*/
+    let max=0;
+    let lookahead=850;
 
-    var max=0;
-    var lookahead=850;
-
-    for(var i=0;i<lookahead;i++){
-        var sample=obj.samples[obj.offset+i]
+    for(let i=0;i<lookahead;i++){
+        let sample=channel.samples[channel.offset+i]
         if(sample>max)
             max=sample;
     }
-
-    for(var i=0;i<10000;i++){
+    /*Try to find the next sync pulse*/
+    for(let i=0;i<max_interimage_samples;i++){
 
         /*Obs, floating point*/
-        if(obj.samples[obj.offset]>=max){
-
+        if(channel.samples[channel.offset]>=max){
             highLevelReachedCounter=60;
         }
         highLevelReachedCounter-=1;
 
-        if(obj.samples[obj.offset]>lowLevel){
+        if(channel.samples[channel.offset]>lowLevel){
             lowCount++;
         }else{
-            /*We have transitioned back to undeer the pulse*/
+            /*We have transitioned back to under the pulse*/
             if((lowCount>triggerCount)&&(highLevelReachedCounter>0)){
                 /*For debuging,indicate were we triggered*/
                 if(false){
-                    obj.samples[obj.offset]=1;
-                    obj.samples[obj.offset+1]=-1;
-                    obj.samples[obj.offset+2]=1;
+                    channel.samples[channel.offset]=1;
+                    channel.samples[channel.offset+1]=-1;
+                    channel.samples[channel.offset+2]=1;
                 }
-                pushLine(obj);
+                pushLine(channel);
                 if(i<500||i>600){
                     // console.log("Found after ",i)
-                    if(obj.haltOnError)
-                        obj.go=false;
+                    if(channel.haltOnError)
+                        channel.go=false;
                 }
-                return obj;
+                return channel;
 
             }
             //Fail
@@ -99,34 +98,34 @@ function nextLine(obj){
             highLevelReachedCounter=0;
         }
 
-        obj.offset+=1;
+        channel.offset+=1;
     }
-    if(obj.haltOnError)
-        obj.go=false;
-    return obj;
+    if(channel.haltOnError)
+        channel.go=false;
+    return channel;
 
 
 }
 
 /*Draws a complete pixel line, the offset has to be set to the end 
  * of a sync pulse when calling this function.*/
-function pushLine(obj){
-    var canvas=document.getElementById(obj.canvasName);
-    var ctx=canvas.getContext("2d");
-    var id=ctx.createImageData(1,1); // only do this once per page
-    var d=id.data;                        // only do this once per page
-    var w=canvas.width,h=canvas.height;
+function pushLine(channel){
+    let canvas=document.getElementById(channel.canvasName);
+    let ctx=canvas.getContext("2d");
+    let id=ctx.createImageData(1,1); // only do this once per page
+    let d=id.data; // only do this once per page
+    let w=canvas.width,h=canvas.height;
 
     /*Shift the prervious rows to make room for a new row, old rows are
      *discarded!*/
-    var imageData=ctx.getImageData(0,0,w,h);
+    let imageData=ctx.getImageData(0,0,w,h);
     ctx.clearRect(0,0,w,h);
     ctx.putImageData(imageData,0,-1);
 
     /*Draw the row, pixel by pixel*/
-    for(var i=0;i<w;i++){
+    for(let i=0;i<w;i++){
         /*This takes some experimentation, the circle is good for this*/
-        var s=108-obj.samples[obj.offset+i]*2555;
+        let s=108-channel.samples[channel.offset+i]*2555;
         d[0]=s;
         d[1]=s;
         d[2]=s;
@@ -135,26 +134,32 @@ function pushLine(obj){
     }
 }
 
-
-
-
-function startDisplayingChannel(obj){
+function startDisplayingChannel(channel){
     setInterval(function(){
-        if(!obj.go)
+        if(!channel.go)
             return;
-        for(var i=0;i<6;i++){
-            obj=nextLine(obj);
+        //Try to find the next pulse
+        for(let i=0;i<6;i++){
+            channel=nextLine(channel);
         }
-        plot(obj.samples,obj.offset,1000,0,1,obj.plotName);
+        plot(channel.samples,channel.offset,1000,0,1,channel.plotName);
 
     },100);
 }
 function onloadCallback(buffer){
-    leftChannel.samples=buffer.getChannelData(0);
-    rightChannel.samples=buffer.getChannelData(1);
-    startDisplayingChannel(leftChannel);
-    startDisplayingChannel(rightChannel);
+    setTimeout(()=>{
+        $("#loading_div").slideUp("slow");
+        leftChannel.samples=buffer.getChannelData(0);
+        rightChannel.samples=buffer.getChannelData(1);
+        startDisplayingChannel(leftChannel);
+        startDisplayingChannel(rightChannel);
+
+    },400)
 }
 
-loadSound("voyager.mp3",onloadCallback);
+function loadingProgress(progress){
+    $("#load_progress").css("width",progress+"%")
+}
+
+loadSound("voyager.mp3",onloadCallback,loadingProgress);
 
